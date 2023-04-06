c012279e5c0863e29d8a492c8a69a7077aa5b654
HeStudy <1467602180@qq.com>
2023-04-06 16:08:42 +0800

    样式调整

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,20 +2,7 @@
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {
-      colors: {
-        background: "#232946",
-        headline: "#fffffe",
-        paragraph: "#b8c1ec",
-        button: "#eebbc3",
-        buttonText: "#232946",
-        stroke: "#121629",
-        main: "#b8c1ec",
-        highlight: "#eebbc3",
-        secondary: "#fffffe",
-        tertiary: "#eebbc3",
-      },
-    },
+    extend: {},
   },
-  plugins: [require("@tailwindcss/typography")],
+  plugins: [require("@tailwindcss/typography"), require("daisyui")],
 };
